1f67ea545c42406e7f7464fdc3b6f9ac0925c9ff
R5meason <melynda.eason@gmail.com>
2017-02-04 09:08:09 -0800

    Added error check count

--- script.js
+++ script.js
@@ -29,6 +29,7 @@ var amtOfTurns = function (turns) {
 function pick_number() {
     var random_number = Math.floor(Math.random() * 10 + 1); //Math.Random gives random # from 0 - .9999[...] Math.floor gives whole number, *10 + 1 multiplies whole number by 10 and adds 1 (in case of zero).
     prizeNum = random_number;
+    console.log(random_number);
     return random_number;
 }
 
@@ -67,63 +68,73 @@ function make_guess() {                 //compares user input to the random numb
 }
 
 
-
-
+var errCount;
 var prizeNum;
 var reward = function () {
-    pick_number();
-    if (prizeNum <= 3) {
-        console.log("quote");
-        $.ajax({
-            url: 'https://andruxnet-random-famous-quotes.p.mashape.com/',
-            datatype: 'json',
-            success: function (data) {
-                var quoteContainer = JSON.parse(data);
-                var quote = "<br>" + "\"" + quoteContainer.quote + "\"" + '<br><span id=\"prizeString\"> -' + quoteContainer.author + '</span>';
-                document.getElementById("randomPrize").innerHTML = quote;
-            },
-            error: function () {
-                reward();
-            },
-            beforeSend: function (xhr) {
-                xhr.setRequestHeader("X-Mashape-Authorization", "035vi91FxRmshxP9HdyDipEnKGr5p15ixpjjsn1IF2377M87v7"); // Enter here your Mashape key
-            }
-        });
-    }
-    else if (prizeNum >= 7) {
-        console.log("img");
-        $.ajax({
-            dataType: 'json',
-            url: 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC',
-            success: function (result) {
-                var randoImg = result.data.fixed_height_downsampled_url;
-                var imgStart = "<img src='";
-                var imgStop = "'>";
-                var logo = "<img id='giphyLogo' src='imgs/Poweredby_100px-White_VertText.png'>";
-                document.getElementById("randomPrize").innerHTML = logo + "<br>" + imgStart + randoImg + imgStop;
-            },
-            error: function () {
-                reward();
-            }
-        });
+    if (errCount === 3) {
+        restart();
     }
     else {
-        console.log("fact");
-        $.ajax({
-            url: 'https://numbersapi.p.mashape.com/' + the_number + '/trivia?fragment=true&json=true&notfound=floor',
-            datatype: 'json',
-            success: function (data) {
-                var triviaNum = "Fact about # " + data.number;
-                var triviaFact = data.number + " is " + data.text;
-                document.getElementById("randomPrize").innerHTML = "<br>" + triviaNum + "<br>" + triviaFact;
-            },
-            error: function () {
-                reward();
-            },
-            beforeSend: function (xhr) {
-                xhr.setRequestHeader("X-Mashape-Authorization", "035vi91FxRmshxP9HdyDipEnKGr5p15ixpjjsn1IF2377M87v7");
-            }
-        });
+        pick_number();
+        if (prizeNum <= 3) {
+            console.log("quote");
+            $.ajax({
+                url: 'https://andruxnet-random-famous-quotes.p.mashape.com/',
+                datatype: 'json',
+                success: function (data) {
+                    var quoteContainer = JSON.parse(data);
+                    var quote = "<br>" + "\"" + quoteContainer.quote + "\"" + '<br><span id=\"prizeString\"> -' + quoteContainer.author + '</span>';
+                    document.getElementById("randomPrize").innerHTML = quote;
+                    errCount = 0;
+                },
+                error: function () {
+                    reward();
+                    errCount += 1;
+                },
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader("X-Mashape-Authorization", "035vi91FxRmshxP9HdyDipEnKGr5p15ixpjjsn1IF2377M87v7"); // Enter here your Mashape key
+                }
+            });
+        }
+        else if (prizeNum >= 7) {
+            console.log("img");
+            $.ajax({
+                dataType: 'json',
+                url: 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC',
+                success: function (result) {
+                    var randoImg = result.data.fixed_height_downsampled_url;
+                    var imgStart = "<img src='";
+                    var imgStop = "'>";
+                    var logo = "<img id='giphyLogo' src='imgs/Poweredby_100px-White_VertText.png'>";
+                    document.getElementById("randomPrize").innerHTML = logo + "<br>" + imgStart + randoImg + imgStop;
+                    errCount = 0;
+                },
+                error: function () {
+                    reward();
+                    errCount += 1;
+                }
+            });
+        }
+        else {
+            console.log("fact");
+            $.ajax({
+                url: 'https://numbersapi.p.mashape.com/' + the_number + '/trivia?fragment=true&json=true&notfound=floor',
+                datatype: 'json',
+                success: function (data) {
+                    var triviaNum = "Fact about # " + data.number;
+                    var triviaFact = data.number + " is " + data.text;
+                    document.getElementById("randomPrize").innerHTML = "<br>" + triviaNum + "<br>" + triviaFact;
+                    errCount = 0;
+                },
+                error: function () {
+                    reward();
+                    errCount += 1;
+                },
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader("X-Mashape-Authorization", "035vi91FxRmshxP9HdyDipEnKGr5p15ixpjjsn1IF2377M87v7");
+                }
+            });
+        }
     }
 };
 
